0a38292b2c2956b28034f82ba0f7f67ebb5903ba
richmartins <richard.martinstenorio@gmail.com>
2020-05-06 12:01:21 +0200

    [DONE] update jekyll + filter cat project + single page per project + modal on single project page

--- js/main.js
+++ js/main.js
@@ -9,4 +9,31 @@ $(document).ready(function () {
         autoplay: true,
         autoplaySpeed: 2000
     });
+
+    $('#project-filter').on('change', function(){
+        var cat = this.value;
+        $('.card').each(function(i){
+            if(cat == 'all'){
+                $(this).removeClass('hidden');
+            }
+            else if(this.className.includes(cat)){
+                $(this).removeClass('hidden');
+            }else{
+                $(this).addClass(" hidden");
+            }
+        })
+    })
+
+    //modal
+    var modal = $('.modal');
+    var toggleModal = $('.project-bottom-imgs > img');
+    toggleModal.on('click', function(){
+        $(this).next('div').css('display', 'block');
+    })
+    modal.on('click', function(){
+        modal.css('display', 'none');
+    })
 });
+
+
+ 
